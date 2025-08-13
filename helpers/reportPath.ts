c118eff962ad19becf00f9d8a974cd1b26d92ce0
Alan <alanputra2002@gmail.com>
2025-08-13 19:30:21 +0700

    Initiate Automation with Playwright, Playwright Test, Typescript

--- helpers/reportPath.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import { getTimestamp } from './getTimestamp.js';
-
-const { date, time } = getTimestamp();
-export const reportFolder = path.join(__dirname,`../playwright-report/Test-Result-${date}_${time}`
-);
